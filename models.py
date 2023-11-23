a74c2fd3d4b16953badc4d61c7aeee4c3a1a4c08
AdiaEra <121941801+AdiaEra@users.noreply.github.com>
2023-11-23 22:33:04 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Text, func
 
-DSN = 'postgresql://postgres:Hun$917&305TpS@localhost:5432/DB'
+DSN = 'postgresql://postgres:postgres@localhost:5432/DB'
 engine = create_engine(DSN)
 atexit.register(engine.dispose)
 
